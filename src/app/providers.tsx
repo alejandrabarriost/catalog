b37c4839a6235fd2d619b81ad9ce3b504c94c55e
alejandrabarriost <93233927+alejandrabarriost@users.noreply.github.com>
2024-01-16 21:09:11 -0500

    Update providers.tsx

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,16 +1,1 @@
-// app/providers.jsx
-"use client";
 
-import {
-  QueryClient,
-  QueryClientProvider as _QueryClientProvider,
-} from "@tanstack/react-query";
-import { PropsWithChildren, useState } from "react";
-
-export default function QueryClientProvider({ children }: PropsWithChildren) {
-  const [queryClient] = useState(() => new QueryClient());
-
-  return (
-    <_QueryClientProvider client={queryClient}>{children}</_QueryClientProvider>
-  );
-}
