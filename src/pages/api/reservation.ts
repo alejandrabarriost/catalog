4e657de4df16e0499401b62b1ce82e39ecdf6bfe
Aleja Barrios <alejandrabarriost@hotmail.com>
2024-01-16 20:46:40 -0500

    added email service

--- src/pages/api/reservation.ts
+++ src/pages/api/reservation.ts
@@ -1,12 +1,24 @@
 import { createReservation } from "@/backend/services/createReservation";
 import { NextApiRequest, NextApiResponse } from "next";
+import { Resend } from "resend";
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
   if (req.method === "POST") {
-    const response = await createReservation(JSON.parse(req.body));
+    const body = JSON.parse(req.body);
+
+    const response = await createReservation(body);
+
+    const resend = new Resend(process.env.RESEND_API_KEY);
+
+    await resend.emails.send({
+      from: "Tools, INC. <onboarding@resend.dev>",
+      to: [body.email],
+      subject: "Reserve Confirmation Received",
+      html: "<strong>Your reserve has been successfully confirmed. thanks for using our service</strong>",
+    });
 
     return res.status(201).json(response);
   }
